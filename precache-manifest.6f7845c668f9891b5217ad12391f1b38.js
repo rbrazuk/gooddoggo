76d46decd13be8a8d879102463ae8d6622261989
Ross Brazuk <ross.brazuk@gmail.com>
2018-10-09 21:29:35 -0400

    Updates

---
+++ precache-manifest.6f7845c668f9891b5217ad12391f1b38.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "36a97a8e81dbc9d078800836656324c2",
+    "url": "/gooddoggo/static/media/doggo2.36a97a8e.jpg"
+  },
+  {
+    "revision": "87e300a65e342cd74185",
+    "url": "/gooddoggo/static/css/main.87a20452.chunk.css"
+  },
+  {
+    "revision": "0b36ef796d4120268b19",
+    "url": "/gooddoggo/static/js/1.0b36ef79.chunk.js"
+  },
+  {
+    "revision": "ae49b5e42df99859cd29",
+    "url": "/gooddoggo/static/js/runtime~main.ae49b5e4.js"
+  },
+  {
+    "revision": "c0f12e24de7812eba57dc21d9eff4834",
+    "url": "/gooddoggo/static/media/logo-light.c0f12e24.png"
+  },
+  {
+    "revision": "87e300a65e342cd74185",
+    "url": "/gooddoggo/static/js/main.87e300a6.chunk.js"
+  },
+  {
+    "revision": "63204342f006b05b7ceda892ec479dc0",
+    "url": "/gooddoggo/static/media/placeholder.63204342.jpg"
+  },
+  {
+    "revision": "8d92740513b63b8ce82593d5937552c8",
+    "url": "/gooddoggo/static/media/header.8d927405.jpg"
+  },
+  {
+    "revision": "deecf200acaa026ba632b0b560c90710",
+    "url": "/gooddoggo/static/media/psi_member_logo_color.deecf200.jpg"
+  },
+  {
+    "revision": "7e892420561e70e4a4521a3d4b1ad8c4",
+    "url": "/gooddoggo/static/media/about.7e892420.jpg"
+  },
+  {
+    "revision": "90591d9f00bad2df6381624ee6ecc17f",
+    "url": "/gooddoggo/index.html"
+  }
+];
